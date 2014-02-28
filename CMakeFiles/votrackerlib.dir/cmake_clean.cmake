fbe99570fb817bc7a0be43276310f85ca5e96b96
Yuhuang Hu <duguyue100@gmail.com>
2014-02-28 14:02:05 +0800

    deleting cmake cache

--- CMakeFiles/votrackerlib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "lib/libvotrackerlib.pdb"
-  "lib/libvotrackerlib.dylib"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/votrackerlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
